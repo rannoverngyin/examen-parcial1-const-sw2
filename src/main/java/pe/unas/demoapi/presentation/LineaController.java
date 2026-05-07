419e6257b2e47531f003b371e917d602e505594a
Leonardo Benjamin Travezanio Rodriguez <leonardo.travezano@unas.edu.pe>
2026-05-07 10:59:03 -0500

    Implementa API ejercicio

---
+++ src/main/java/pe/unas/demoapi/presentation/LineaController.java
@@ -0,0 +1,22 @@
+package pe.unas.demoapi.presentation;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import pe.unas.demoapi.aplication.LineaService;
+
+
+@RestController
+public class LineaController {
+    private final LineaService service;
+    public LineaController(LineaService service){
+        this.service = service;
+    }
+    @GetMapping("/lineas")
+        public List<String> listar(){
+            return service.listar();
+        }
+    }
+
