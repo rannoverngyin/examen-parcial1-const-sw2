2894ac318e763599f1b6b0d09cab797b0c66ab3a
Leonardo Benjamin Travezanio Rodriguez <leonardo.travezano@unas.edu.pe>
2026-05-12 10:01:43 -0500

    Implementa pruebas unitarias con TDD

---
+++ src/test/java/pe/unas/demoapi/CalculadoraServiceTest.java
@@ -0,0 +1,45 @@
+package pe.unas.demoapi;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+
+import pe.unas.demoapi.application.CalculadoraService;
+public class CalculadoraServiceTest {
+    CalculadoraService service =
+            new CalculadoraService();
+    @Test
+    void sumar(){
+        assertEquals(
+                4,
+                service.sumar(2,2)
+        );
+    }
+    @Test
+    void restar(){
+        assertEquals(
+                2,
+                service.restar(5,3)
+        );
+    }
+    @Test
+    void dividir(){
+        assertEquals(
+                2,
+                service.dividir(4,2)
+        );
+    }
+    @Test
+    void dividirPorCero(){
+        assertThrows(
+                ArithmeticException.class,
+                () -> service.dividir(4,0)
+        );
+    }
+    @Test
+    void multiplicar() {
+        assertEquals(
+                15,
+                service.multiplicar(3, 5)
+        );
+    }
+}
---
+++ src/test/java/pe/unas/demoapi/DemoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.unas.demoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
