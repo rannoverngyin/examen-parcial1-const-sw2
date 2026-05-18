1014d9e05051cbc7f56c8bc36405e2b5eb1c3a1e
Nilver L <nilver.carhuapoma@unas.edu.pe>
2026-05-17 22:13:19 -0500

    Implementa pruebas unitarias con TDD

---
+++ src/test/java/pe/unas/demoapi/application/CalculadoraServiceTest.java
@@ -0,0 +1,35 @@
+package pe.unas.demoapi.application;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculadoraServiceTest {
+    CalculadoraService service = new CalculadoraService();
+
+    @Test
+    void sumar(){
+        assertEquals(4, service.sumar(2,2));
+    }
+    @Test
+    void restar(){
+        assertEquals(2, service.restar(5, 3));
+    }
+    @Test
+    void dividir(){
+        assertEquals(2, service.dividir(4, 2));
+    }
+    @Test
+    void dividirPorCero(){
+        assertThrows(ArithmeticException.class ,
+            ()->service.dividir(4,0)
+        );
+    }
+    @Test
+    void multiplicar (){
+        assertEquals(12, service.multiplicar(4, 3));
+    }
+
+
+}
