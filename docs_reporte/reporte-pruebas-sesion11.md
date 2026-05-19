c34ac8cf90b41bf104bb651dc7edd0517bf13431
FABIO SEBASTIAN UPIACHIHUA LOZANO <fabio.upiachihua@unas.edu.pe>
2026-05-19 00:59:51 -0500

    Agrega pruebas de integracion y concurrencia

---
+++ docs_reporte/reporte-pruebas-sesion11.md
@@ -0,0 +1,25 @@
+# Reporte de Pruebas - Sesión 11
+
+## Pruebas ejecutadas
+
+- ProductoControllerIntegrationTest
+- ProductoServiceConcurrencyTest
+- ProductoApiConcurrencyTest
+
+---
+
+## Resultado
+
+![alt text](image.png)
+
+---
+
+## Evidencias
+
+- mvn test
+![alt text](image-1.png)
+- endpoint /productos
+![alt text](image-2.png)
+---
+
+![alt text](image-3.png)
---
+++ src/main/java/pe/unas/demoapi/application/ProductoService.java
@@ -0,0 +1,45 @@
+package pe.unas.demoapi.application;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@Service
+public class ProductoService {
+
+    private final List<String> productos =
+            new CopyOnWriteArrayList<>();
+
+    public ProductoService() {
+
+        productos.add("Laptop");
+        productos.add("Mouse");
+
+    }
+
+    public List<String> listar() {
+
+        return productos;
+
+    }
+
+    public void agregar(String nombre){
+
+        productos.add(nombre);
+
+    }
+
+    public void eliminar(String nombre){
+
+        productos.remove(nombre);
+
+    }
+
+    public int total(){
+
+        return productos.size();
+
+    }
+
+}
