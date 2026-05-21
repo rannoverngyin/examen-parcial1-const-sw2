b02493aed1cdc7ace9f015219130b769bb6ca186
Brando <brando.benavides@unas.edu.pe>
2026-05-20 22:59:04 -0500

    Mantiene banco de pruebas de ProductoService

--- src/main/java/pe/unas/demoapi13/Demoapi13Application.java
+++ src/main/java/pe/unas/demoapi13/Demoapi13Application.java
@@ -1,13 +1,13 @@
-package pe.unas.demoapi;
+package pe.unas.demoapi13;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ExamenParcial1ConstSw2Application {
+public class Demoapi13Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ExamenParcial1ConstSw2Application.class, args);
+		SpringApplication.run(Demoapi13Application.class, args);
 	}
 
 }
