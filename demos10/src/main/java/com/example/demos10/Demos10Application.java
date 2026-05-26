cbdb09f586a171173837b9a772554396d24f8ffb
Brando <brando.benavides@unas.edu.pe>
2026-05-26 08:29:56 -0500

    reprobamos

---
+++ demos10/src/main/java/com/example/demos10/Demos10Application.java
@@ -0,0 +1,13 @@
+package com.example.demos10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demos10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demos10Application.class, args);
+	}
+
+}
---
+++ demos10/src/test/java/com/example/demos10/Demos10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demos10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demos10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
