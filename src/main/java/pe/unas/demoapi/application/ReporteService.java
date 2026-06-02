ee644e6990e1eed48cf60272bc0453fc8a90e68b
Nilver L <nilver.carhuapoma@unas.edu.pe>
2026-06-02 10:26:32 -0500

    medio_curso

--- src/main/java/pe/unas/demoapi/application/ReporteService.java
+++ src/main/java/pe/unas/demoapi/application/ReporteService.java
@@ -1,16 +1,9 @@
 package pe.unas.demoapi.application;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 @Service
 public class ReporteService {
 
-    
-
-    
-    public String generar(){
+     public String generar(){
             return "Reporte generado";
         }
         
