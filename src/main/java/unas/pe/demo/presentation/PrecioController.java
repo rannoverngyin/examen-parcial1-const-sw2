50ab7b04d755ffb5851d47a9a767c965372a6179
diegoLeon24 <diego.leon@unas.edu.pe>
2026-06-04 07:47:07 -0500

    Implementa analisis de variabilidad y agrega variante ESTUDIANTE con pruebas unitarias exitosas

---
+++ src/main/java/unas/pe/demo/presentation/PrecioController.java
@@ -0,0 +1,27 @@
+package unas.pe.demo.presentation;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import unas.pe.demo.application.PrecioService;
+
+@RestController
+public class PrecioController {
+
+    private final PrecioService service;
+
+    public PrecioController(PrecioService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/precio-final")
+    public double calcular(@RequestParam double precio) {
+        return service.calcularPrecioFinal(precio);
+    }
+
+    @GetMapping("/variante-activa")
+    public String variante() {
+        return service.obtenerVarianteActiva();
+    }
+}
---
+++ src/test/java/unas/pe/demo/PrecioServiceTest.java
@@ -0,0 +1,31 @@
+package unas.pe.demo;
+
+
+import org.junit.jupiter.api.Test;
+import unas.pe.demo.application.PrecioService;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PrecioServiceTest {
+
+    private final PrecioService service = new PrecioService();
+
+    @Test
+    void debeAplicarDescuentoPremium() {
+        assertEquals(90.0, service.calcularPorVariante(100, "PREMIUM"));
+    }
+
+    @Test
+    void debeAplicarDescuentoVip() {
+        assertEquals(80.0, service.calcularPorVariante(100, "VIP"));
+    }
+
+    @Test
+    void debeAplicarDescuentoEstudiante() {
+        assertEquals(70.0, service.calcularPorVariante(100, "ESTUDIANTE"));
+    }
+
+    @Test
+    void debeMantenerPrecioBasico() {
+        assertEquals(100.0, service.calcularPorVariante(100, "BASICO"));
+    }
+}
