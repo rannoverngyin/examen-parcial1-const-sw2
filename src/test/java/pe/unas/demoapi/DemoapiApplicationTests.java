aab68edcfd8b0994db8c7c11037bd70b3f1ec1c8
Kevin Escally <escallyk@gmail.com>
2026-06-04 10:32:30 -0500

    Sesion 18 Configuracion de perfiles

---
+++ src/test/java/pe/unas/demoapi/DemoapiApplicationTests.java
@@ -0,0 +1,14 @@
+package pe.unas.demoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("dev")
+class DemoapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
