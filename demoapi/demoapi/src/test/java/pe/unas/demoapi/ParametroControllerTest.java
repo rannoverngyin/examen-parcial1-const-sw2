81a7256b60a1c6d1a46b876b025f7092121154c5
Leonardo Benjamin Travezanio Rodriguez <leonardo.travezano@unas.edu.pe>
2026-06-09 09:47:59 -0500

    Implementa configuracion, parametrizacion e inyeccion de dependencias sesion 19

---
+++ demoapi/demoapi/src/test/java/pe/unas/demoapi/ParametroControllerTest.java
@@ -0,0 +1,26 @@
+package pe.unas.demoapi;
+
+import static org.hamcrest.Matchers.containsString;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ParametroControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void debeMostrarVersionConfigurada() throws Exception {
+        mockMvc.perform(get("/parametros/version"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("1.0.0")));
+    }
+}
