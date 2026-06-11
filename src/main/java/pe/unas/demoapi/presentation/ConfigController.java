a5e78112e9d8febe4f3067f3ddd54d6d74d10507
Kevin Escally <escallyk@gmail.com>
2026-06-11 09:32:33 -0500

    sesion 20

---
+++ src/main/java/pe/unas/demoapi/presentation/ConfigController.java
@@ -0,0 +1,30 @@
+package pe.unas.demoapi.presentation;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/config")
+public class ConfigController {
+
+    @Value("${spring.profiles.active:default}")
+    private String activeProfile;
+
+    @GetMapping("/info")
+    public ResponseEntity<Map<String, String>> info() {
+        Map<String, String> info = new LinkedHashMap<>();
+        info.put("aplicacion", "examen-parcial1-const-sw2");
+        info.put("version", "1.0.0");
+        info.put("perfil_activo", activeProfile);
+        info.put("java_version", System.getProperty("java.version"));
+        info.put("curso", "Construccion de Software II - UNAS");
+        info.put("sesion", "Sesion 20 - Contenedores y Despliegue");
+        return ResponseEntity.ok(info);
+    }
+}
---
+++ src/test/java/pe/unas/demoapi/ProductoControllerTest.java
@@ -0,0 +1,61 @@
+package pe.unas.demoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ProductoControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void listarProductos_retorna200() throws Exception {
+        mockMvc.perform(get("/productos"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    void obtenerProducto_existente_retorna200() throws Exception {
+        mockMvc.perform(get("/productos/1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void obtenerProducto_noExistente_retorna404() throws Exception {
+        mockMvc.perform(get("/productos/9999"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void crearProducto_retorna201() throws Exception {
+        String json = """
+                {
+                    "nombre": "Producto Test",
+                    "descripcion": "Descripcion test",
+                    "precio": 10.00,
+                    "stock": 5
+                }
+                """;
+        mockMvc.perform(post("/productos")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void configInfo_retorna200() throws Exception {
+        mockMvc.perform(get("/config/info"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.aplicacion").value("examen-parcial1-const-sw2"));
+    }
+}
