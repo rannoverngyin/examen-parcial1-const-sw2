f53651d0aa99e5e9a81fcd2e130fc0834a4e4f34
FABIO SEBASTIAN UPIACHIHUA LOZANO <fabio.upiachihua@unas.edu.pe>
2026-06-10 22:45:57 -0500

    Dockeriza aplicacion Spring Boot

---
+++ src/test/java/pe/unas/demoapi/ExamenParcial1ConstSw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.unas.demoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenParcial1ConstSw2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
