dbc15367897f4521bb6f20f146b7cae6f18288e6
LuisLara2 <luis.lara@unas.edu.pe>
2026-07-14 10:59:47 -0500

    Diseña escenarios avanzados de carga y agrega reporte con metricas S29

--- performance/sesion29/escenario-pico-doble.js
+++ performance/sesion29/escenario-pico-doble.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { check, group, sleep } from 'k6';
 import { Counter, Rate, Trend } from 'k6/metrics';
 
-const BASE_URL = __ENV.BASE_URL || 'http://localhost:8080';
+const BASE_URL = __ENV.BASE_URL || 'http://host.docker.internal:8080';
 
 const businessErrors = new Counter('business_errors');
 const successfulWrites = new Rate('successful_writes');
